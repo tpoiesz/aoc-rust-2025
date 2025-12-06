11956ce07510f3024780678b6b2ba839cfec74e9
Thomas Poiesz <ta.poiesz@gmail.com>
2025-12-06 23:46:39 +0100

    optimize day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -30,45 +30,50 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut problems = Vec::new();
-
-    let lines = input.lines().collect::<Vec<&str>>();
-    let numbers = lines[..lines.len() - 1].iter().map(|s| s.as_bytes()).collect::<Vec<_>>();
-    let num_cols = numbers.iter().max_by_key(|l| l.len()).unwrap().len();
-    let num_rows = numbers.len();
-    let mut number_index = 0;
-    problems.push(Vec::new());
-    for column in 0..num_cols {
-        let mut num = None;
-        for row in 0..num_rows {
-            let maybe_char = numbers[row].get(column);
-            if maybe_char.is_some_and(|c| c.is_ascii_digit()) {
-                let digit = (maybe_char.unwrap() - b'0') as u64;
-                num = num.map(|n| 10*n + digit).or(Some(digit));
-            }
-        }
-        match num {
-            None => {
-                number_index += 1;
-                problems.push(Vec::new());
-            }
-            Some(n) => { problems[number_index].push(n) }
-        }
-    }
-
-    // println!("{:?}", problems);
-
+    let mut lines = input.lines().map(str::as_bytes).collect::<Vec<_>>();
+    let operator_line = lines.pop().unwrap();
     let mut result = 0;
 
-    for (idx, operator) in lines.last().unwrap().split_ascii_whitespace().enumerate() {
-        match operator {
-            "+" => result += problems[idx].iter().sum::<u64>(),
-            "*" => result += problems[idx].iter().product::<u64>(),
-            _ => panic!(),
+    let num_cols = lines.iter().map(|line| line.len()).max().unwrap();
+    let mut operator = b'.';
+
+    let mut problem_outcome = None;
+    for column in 0..num_cols {
+        if problem_outcome.is_none() {
+            operator = operator_line[column];
+        }
+
+        let number_in_column = parse_column(&lines, column);
+
+        // If num is None the column was empty so we reached the end of the current problem
+        match number_in_column {
+            None => {
+                result += problem_outcome.unwrap();
+                problem_outcome = None;
+            },
+            Some(n) => problem_outcome = problem_outcome.map(|current| match operator {
+                b'+' => current + n,
+                b'*' => current * n,
+                _ => panic!(),
+            }).or(Some(n)),
         }
     }
+    // For latest problem we never reach an empty column so adjust result with final problem outcome
+    result += problem_outcome.unwrap();
 
     Some(result)
+}
+
+fn parse_column(lines: &Vec<&[u8]>, column: usize) -> Option<u64> {
+    let mut column_number = None;
+    for row in 0..lines.len() {
+        let maybe_char = lines[row].get(column); //use Option because not all rows are equally long
+        if maybe_char.is_some_and(|c| c.is_ascii_digit()) {
+            let digit = (maybe_char.unwrap() - b'0') as u64;
+            column_number = column_number.map(|n| 10 * n + digit).or(Some(digit));
+        }
+    }
+    column_number
 }
 
 #[cfg(test)]
